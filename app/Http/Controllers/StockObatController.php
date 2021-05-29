b963af99da87c9e16be4255acdad893d6995b62e
Rizki Tirta Buwono <rizkipou01@gmail.com>
2021-05-29 17:22:03 +0700

    Validasi Stock Obat

--- app/Http/Controllers/StockObatController.php
+++ app/Http/Controllers/StockObatController.php
@@ -7,6 +7,7 @@ use App\Models\StockObat;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
 class StockObatController extends Controller
 {
@@ -30,6 +31,33 @@ class StockObatController extends Controller
 
     public function store(Request $request)
     {
+        $rules = [
+            'obat_id' => 'required',
+            'masuk' => 'required',
+            'keluar' => 'required',
+            'jual' => 'required',
+            'beli' => 'required',
+            'expired' => 'required',
+            'stock_akhir' => 'required',
+            'keterangan' => 'required',
+        ];
+
+        $message = [
+            'obat_id.required' => 'Obat Tidak Boleh Kosong!',
+            'masuk.required' => 'Kolom stok masuk Tidak Boleh Kosong!',
+            'keluar.required' => 'Kolom stok keluar Tidak Boleh Kosong!',
+            'jual.required' => 'Kolom harga jual Tidak Boleh Kosong!',
+            'beli.required' => 'Kolom harga beli Tidak Boleh Kosong!',
+            'expired.required' => 'Kolom expired Tidak Boleh Kosong!',
+            'stock_akhir.required' => 'Kolom stock akhir Tidak Boleh Kosong!',
+            'keterangan.required' => 'Kolom Keterangan Tidak Boleh Kosong!',
+        ];
+
+        $validasi = Validator::make($request->all(), $rules, $message);
+        if ($validasi->fails()) {
+            return response()->json(['message' => $validasi->errors()->first()], 422);
+        }
+
         $data = new StockObat();
         $data->obat_id = $request->obat_id;
         $data->masuk = $request->masuk;
